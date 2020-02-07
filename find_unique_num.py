cebab12a9e647906f0df5e8582098e6206c1ac0a
Nick <nicholaswood89@gmail.com>
2020-02-07 14:23:33 -0500

    updated optimal solution for unique number

--- find_unique_num.py
+++ find_unique_num.py
@@ -13,17 +13,36 @@
 #     Find the unique string
 #     Find The Unique
 
-def find_uniq(arr):
+def find_uniq(arr): #optimal solution - big o of n
+    if arr[0] != arr[1] and arr[0] != arr[2]:
+        return arr[0]
+    else:
+        counter = 1
+        for i in arr:
+            if i ==  arr[counter]:
+                counter += 1
+            else:
+                return arr[counter]
+
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+print(find_uniq([0,1,1,1,1]))
+print(find_uniq([7,9,9,9,9,9]))
+print(find_uniq([9,7,9,9,9,9,9]))
+
+# better but not optimal
+def find_uniq2(arr): #big o of nlogn 
     arr = sorted(arr)
     if arr[0] != arr[1]:
         return arr[0]
     else:
         return arr[-1]
-print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
-print(find_uniq([ 0, 0, 0.55, 0, 0]))
+# print(find_uniq2([ 1, 1, 1, 2, 1, 1 ]))
+# print(find_uniq2([ 0, 0, 0.55, 0, 0]))
 
 # the below works but is times out on large datasets
-# def find_uniq(arr):
-#     for n in arr:
-#         if arr.count(n) == 1:
-#             return n
+def find_uniq1(arr): #big o of n^2
+    for n in arr:
+        if arr.count(n) == 1:
+            return n
+# print(find_uniq1([ 1, 1, 1, 2, 1, 1 ]))
+# print(find_uniq1([ 0, 0, 0.55, 0, 0]))
